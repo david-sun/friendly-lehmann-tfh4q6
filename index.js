7922d4ee2a924e0ed1b419e5b223202d7ca3b448
CodeSandbox <support@codesandbox.io>
2023-08-09 19:49:04 +0000

    Initial commit

--- index.js
+++ index.js
@@ -3,8 +3,50 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    console.log("this is a test code");
-    res.end(); //end the response
+    // https://frontendguruji.com/blog/how-to-parse-post-request-in-node-js-without-expressjs-body-parser/
+    let chunks = [];
+    // 'data' event is emitted on every chunk received
+    req.on("data", (chunk) => {
+      // collecting the chunks in array
+      chunks.push(chunk);
+    });
+
+    // when all chunks are received, 'end' event is emitted.
+    req.on("end", () => {
+      // joining all the chunks received
+      const data = Buffer.concat(chunks);
+      // data.toString() converts Buffer data to querystring format
+      const querystring = data.toString();
+      // URLSearchParams: takes querystring
+      // & returns a URLSearchParams object instance.
+      const parsedData = new URLSearchParams(querystring);
+      const dataObj = {};
+      // entries() method returns an iterator
+      // allowing iteration through all key/value pairs
+      for (var pair of parsedData.entries()) {
+        dataObj[pair[0]] = pair[1];
+      }
+      // Now request data is accessible using dataObj
+
+      // console.log(JSON.stringify(dataObj));
+      if (dataObj.log) {
+        console.log("  LOG: " + dataObj.log);
+      } else if (dataObj.warn) {
+        console.warn(" WARN: " + dataObj.warn);
+      } else if (dataObj.error) {
+        console.error("ERROR: " + dataObj.error);
+      }
+
+      res.write("You log is displayed @ https://tfh4q6-8080.csb.app/"); //write a response to the client
+      res.end(); //end the response
+
+      // console.log(">>>>> all set");
+
+      res.end();
+    });
+
+    // res.write("Hello from CodeSandbox!"); //write a response to the client
+    // res.end(); //end the response
+    // console.log(">>>>> all set");
   })
   .listen(8080); //the server object listens on port 8080
